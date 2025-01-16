cf07a449efdd8ea1b44df73002cb7f6728407859
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-01-16 19:05:23 +0530

    Map Methods

--- JS/MajorJS/Map.js
+++ JS/MajorJS/Map.js
@@ -11,3 +11,27 @@ for (let i of arr) {
 }
 
 console.log(freq);
+
+
+const user = new Map();
+user.set('id', 1)
+user.set('name', 'Aditya')
+user.set('role', 'Developer')
+
+console.log(user.get('name'));
+
+user.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+})
+
+if (user.has('name')) {
+    user.delete('name')
+}
+
+if (user.has('role')) {
+    console.log(user.get('role'))
+}
+
+console.log(user.size); //2
+
+console.log(user)
