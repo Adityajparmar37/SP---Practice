9f8dc1670a765d7bf1704841d8fc466a7dab27b1
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-01-17 10:03:56 +0530

    Set methods

--- JS/MajorJS/Set.js
+++ JS/MajorJS/Set.js
@@ -2,9 +2,24 @@ const unique = new Set([1, 2, 3, 4, 5, 6, 7, 8, 7, 7]);
 console.log( unique.add({ name: "Aditya" }))
 console.log(unique.add({ name: "Aditya" }))
 console.log(unique.add({ name: "Parmar" }))
+
+
+unique.forEach((value) => {
+    console.log(value)
+})
+
+for (const i of unique) {
+    console.log(i);
+}
+
+console.log(unique.values());
+
+
+
 console.log(unique.has(-1))
 console.log(unique.has(8))
 console.log(unique.delete(-1))
 console.log(unique.delete(5))
 console.log(unique.clear())
 console.log(unique)
+
