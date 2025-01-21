bc6f5fe56c13482cf04b1bc628ce1d5731fa0f33
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-01-21 11:24:18 +0530

    Infinity Currying

--- JS/MajorJS/Currying.js
+++ JS/MajorJS/Currying.js
@@ -10,3 +10,18 @@ const res1 = level1(1);
 const res2 = res1(2);
 const final = res2(3);
 console.log(final);
+
+
+// infinity currying
+
+const sum = (a) => {
+  return function (b) {
+    if (b) {
+      return sum(a+b)
+    } else {
+      return a;
+    }
+  }
+}
+
+console.log(sum(1)(2)(3)(4)());
