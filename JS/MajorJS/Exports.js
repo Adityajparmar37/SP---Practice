00b2b31fa8f5a73efd660a0f9653bc4c34ba3378
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-01-31 18:28:35 +0530

    init koaJS

--- JS/MajorJS/Exports.js
+++
@@ -1,7 +0,0 @@
-export const add = (a, b) => a + b;
-// const subtract = (a, b) => a - b;
-export const mul = (a, b) => a * b;
-
-// module.exports = {
-//     subtract,
-// }
--- JS/MajorJS/Imports.js
+++
@@ -1,7 +0,0 @@
-import { add, mul } from "./Exports.js";
-
-// const {subtract} = require("./Exports.js")
-
-console.log(add(2, 3));
-console.log(mul(2, 3));
-// console.log(subtract(2, 3));
