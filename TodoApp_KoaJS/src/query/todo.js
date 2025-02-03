80df5be0a96f766ff8a6f51a6f9d812d673d1f81
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-02-03 17:06:17 +0530

    remove db and collection name

--- TodoApp_KoaJS/src/query/todo.js
+++ TodoApp_KoaJS/src/query/todo.js
@@ -2,8 +2,8 @@ import { client } from "../../config/connectDb.js";
 
 export const findTodos = async (filter, sortOrder = 1) => {
   const result = await client
-    .db("todoDb")
-    .collection("todos")
+    .db(process.env.DATABASE)
+    .collection(process.env.TODO_COLLECTION)
     .find(filter)
     .sort({ priority: sortOrder })
     .toArray();
@@ -17,25 +17,24 @@ export const insertTodo = async (data) => {
 
 export const removeTodoById = async (todoId) => {
   const result = await client
-    .db("todoDb")
-    .collection("todos")
+    .db(process.env.DATABASE)
+    .collection(process.env.TODO_COLLECTION)
     .deleteOne({ _id: todoId });
   return result;
 };
 
 export const updateTodoById = async (todoId, updateTodoData) => {
   const result = await client
-    .db("todoDb")
-    .collection("todos")
+    .db(process.env.DATABASE)
+    .collection(process.env.TODO_COLLECTION)
     .updateOne({ _id: todoId }, { $set: updateTodoData });
   return result;
 };
 
 export const findTodoById = async (filter) => {
   const result = await client
-    .db("todoDb")
-    .collection("todos")
-    .findOne(filter)
+    .db(process.env.DATABASE)
+    .collection(process.env.TODO_COLLECTION)
+    .findOne(filter);
   return result;
 };
-
