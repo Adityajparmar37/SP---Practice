5d22c93b260916b7b5ec5eb6c6bf7c9b81189a89
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-02-04 14:23:54 +0530

    added koa error handler for async

--- TodoApp_KoaJS/src/middleware/handleAsync.js
+++ TodoApp_KoaJS/src/middleware/handleAsync.js
@@ -2,7 +2,8 @@ export const handleAsync = (controller) => async (ctx, next) => {
   try {
     await controller(ctx, next);
   } catch (error) {
-    console.log("error", error);
-    await next(error);
+    console.error("Error caught:", error);
+    ctx.status = error.status || 500;
+    ctx.body = { error: error.message || "Internal Server Error" };
   }
 };
