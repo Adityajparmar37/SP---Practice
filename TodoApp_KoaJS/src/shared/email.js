8263eb0e24d60c46e8ab63c72cb5077cebecd426
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2025-02-11 11:22:37 +0530

    email validation length 50

--- TodoApp_KoaJS/src/shared/email.js
+++ TodoApp_KoaJS/src/shared/email.js
@@ -1,5 +1,5 @@
 export const isEmailValid = (email) => {
   const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$/;
-  if (!emailRegex.test(email) || email.length >= 320) return false;
+  if (!emailRegex.test(email) || email.length >= 50) return false;
   return true;
 };
